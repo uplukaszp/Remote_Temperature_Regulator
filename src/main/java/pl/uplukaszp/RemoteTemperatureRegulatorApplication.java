5977544ba9d8abd874a168081a3eb5a716115b36
Łukasz <uplukaszp1@gmail.com>
2018-10-13 13:44:23 +0200

    Init commit

---
+++ src/main/java/pl/uplukaszp/RemoteTemperatureRegulatorApplication.java
@@ -0,0 +1,12 @@
+package pl.uplukaszp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTemperatureRegulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTemperatureRegulatorApplication.class, args);
+	}
+}
